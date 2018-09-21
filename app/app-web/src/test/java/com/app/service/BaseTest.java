8020519a3253e2028836667fd16858fe80d28fa2
wangtw <wangtw@qq.com>
2018-09-21 20:26:36 +0800

    javabean xml 互转
    Signed-off-by: wangtw <wangtw@qq.com>

--- app/app-web/src/test/java/com/app/service/BaseTest.java
+++ app/app-web/src/test/java/com/app/service/BaseTest.java
@@ -7,6 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.app.bankbean.BankBean01In;
+import com.app.bankbean.BankBean01Out;
+import com.app.bankbean.BankBeanPubRequest;
+import com.app.bankbean.BankBeanRequest;
+import com.app.bankbean.BankBeanResponse;
+import com.app.util.JaxbUtil;
+
 /**
  * 测试基类 . <br>
  * SpringBootTest.WebEnvironment.RANDOM_POR 表示使用随机端口号
@@ -18,5 +25,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Ignore
 public class BaseTest {
+    public static void main(String[] args) {
+    	System.out.println("=================javabean to xml=============");
+    	BankBeanPubRequest bbpr = new BankBeanPubRequest();
+    	bbpr.setTransCode("0000");
+    	bbpr.setBankCode("1111");
+    	BankBean01In bbi = new BankBean01In();
+    	bbi.setAccNo("1234567890");
+    	bbi.setAreaCode("10");
+    	BankBeanRequest<BankBean01In> bbRequest = new BankBeanRequest<BankBean01In>(bbpr,bbi);
+    	String xml = JaxbUtil.convertToXml(BankBeanRequest.class, bbRequest);
+    	System.out.println(xml);
 
+    	System.out.println("=================xml to javabean=============");
+    	String xmlResponse = "<?xml version=\"1.0\" encoding = \"GBK\"?>\r\n" + 
+    			"<CMS>\r\n" + 
+    			"<eb>\r\n" + 
+    			"    <pub>\r\n" + 
+    			"        <TransCode>0000</TransCode>\r\n" + 
+    			"        <BankCode>1111</BankCode>\r\n" + 
+    			"    </pub>\r\n" + 
+    			"    <out>\r\n" + 
+    			"        <AccNo>1234567890</AccNo>\r\n" + 
+    			"        <AreaCode>10</AreaCode>\r\n" + 
+    			"        <rd>\r\n" + 
+    			"            <Drcrf>1</Drcrf>\r\n" + 
+    			"            <VouhNo>2343264234</VouhNo>\r\n" + 
+    			"        </rd>\r\n" + 
+    			"        <rd>\r\n" + 
+    			"            <Drcrf>2</Drcrf>\r\n" + 
+    			"            <VouhNo>867867867</VouhNo>\r\n" + 
+    			"        </rd>\r\n" + 
+    			"    </out>\r\n" + 
+    			"</eb>\r\n" + 
+    			"</CMS>";
+    	@SuppressWarnings("unchecked")
+		BankBeanResponse<BankBean01Out> bbResponse = JaxbUtil.convertToJavaBean(BankBeanResponse.class, xmlResponse);
+    	System.out.println(bbResponse.getPub().getBankCode());
+    	System.out.println(bbResponse.getOut().getAccNo());
+    }
 }
